d648283c594f767fa9e6c012ab76f2b358e5acb6
jaemin-i <daller10000@gmail.com>
2023-11-22 23:09:18 +0900

    kinect_project

---
+++ kinectsource/MainWindow.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace kinectsource
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Page
+    {
+        MySqlCommand selectCommand;
+        MySqlDataReader resultInfo;
+        MySqlConnection conn;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        public void openDB()
+        {
+            try
+            {
+                conn = new MySqlConnection("Server=192.168.219.101;Database=project;Uid=test;Pwd=test1234");
+                conn.Open();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("DB연결실패" + e.ToString());
+            }
+        }
+
+        public bool dateUpdate(string id)
+        {
+            openDB();
+            DateTime dt = new DateTime();
+            string lastLoginTime = dt.ToString("yyyy-MM-dd");
+            try
+            {
+                string sql = "UPDATE login SET last_login =\'" + lastLoginTime + "\' WHERE = id =\'" + id + "\'";
+                selectCommand = new MySqlCommand(sql, conn);
+                if (selectCommand.ExecuteNonQuery() != 1)
+                {
+                    conn.Close();
+
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("DB업데이트 실패" + e.ToString());
+                conn.Close();
+
+            }
+            conn.Close();
+            return true;
+        }
+        public void startSelect(string id)
+        {
+            openDB();
+            string sql = "SELECT name, phone_num, email FROM member WHERE id = \'" + id + "\'";
+            try
+            {
+                selectCommand = new MySqlCommand(sql, conn);
+                resultInfo = selectCommand.ExecuteReader();
+                if (resultInfo.Read())
+                {
+
+                    string name = resultInfo["name"].ToString();
+                    string phone = resultInfo["phone_num"].ToString();
+                    string email = resultInfo["email"].ToString();
+
+                    MessageBox.Show("로그인 성공!");
+                    conn.Close();
+                    cctv test = new cctv(name, phone, email); // 화면전환시 이름, 전화번호, 이메일을 CCTV로 보낸다.
+                    NavigationService.Navigate(test);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("DB 정보 검색 실패" + e.ToString());
+            }
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)//로그인버튼 클릭했을때발생
+        {
+            openDB();
+            if (tbxId.Text == "")//아이디 공백 확인
+            {
+                MessageBox.Show("아이디를 입력해주세요.");
+                tbxId.Focus();
+                return;
+            }
+            else if (tbxPs.Password == "")//비밀번호 공백 확인
+            {
+                MessageBox.Show("비밀번호를 입력해주세요.");
+                tbxPs.Focus();
+                return;
+            }
+            try
+            {
+                string loginid = tbxId.Text;
+                string loginpw = tbxPs.Password;
+                string sql = "select * from login where id = \'" + loginid + "\' and password = sha2(\'" + loginpw + "\',256)";
+
+                selectCommand = new MySqlCommand(sql, conn);
+                resultInfo = selectCommand.ExecuteReader();
+
+                //if(tbxId.Text == "admin" && tbxPs.Password == "admin")//아이디비번 일치 확인(db연동부분)
+                if (resultInfo.Read())
+                {
+                    conn.Close();
+                    if (!dateUpdate(loginid))
+                    {
+                        return;
+                    }
+                    startSelect(loginid);
+                }
+                else
+                {
+                    MessageBox.Show("아이디와 비밀번호가 일치하지 않습니다.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("DB 로그인 정보 조회 실패" + ex.ToString());
+            }
+
+
+        }
+
+        private void TbxId_KeyDown(object sender, KeyEventArgs e)//아이디박스에서 엔터눌렀을경우발생
+        {
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                BtnLogin_Click(sender, e);
+            }
+        }
+
+        private void TbxPs_KeyDown(object sender, KeyEventArgs e)//패스워드박스에서 엔터눌렀을경우발생
+        {
+            if (e.Key == System.Windows.Input.Key.Enter)
+            {
+                BtnLogin_Click(sender, e);
+            }
+        }
+    }
+}
---
+++ kinectsource/cctv.xaml.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Kinect;
+
+
+namespace kinectsource
+{
+    /// <summary>
+    /// cctv.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class cctv : Page
+    {
+        double detect = 3;
+        string name;
+        string phone;
+        string email;
+
+        public cctv()
+        {
+
+
+        }
+        public cctv(string name, string phone, string email)
+        {
+            this.name = name;
+            this.phone = phone;
+            this.email = email;
+            InitializeComponent();
+            // InitializeKinect();
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(100);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+        void timer_Tick(object sender, EventArgs e)
+        {
+            txtTime.Text = "시스템시간 : " + DateTime.Now.ToString("yy.MM.dd HH:mm");
+        }
+
+        KinectSensor objKS = null;
+
+        private void InitializeKinect()
+        {
+            try
+            {
+                objKS = KinectSensor.KinectSensors[0];
+                objKS.ColorStream.Enable();
+                objKS.ColorFrameReady += new EventHandler<ColorImageFrameReadyEventArgs>(objKS_ColorFrameReady);
+
+                objKS.DepthStream.Enable();
+                objKS.SkeletonStream.Enable();
+                objKS.AllFramesReady += new EventHandler<AllFramesReadyEventArgs>(objKS_AllFramesReady);
+                objKS.Start();
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                MessageBox.Show("카메라가 연결되어있지않습니다.\n프로그램을 종료합니다.");
+            }
+
+
+        }
+
+        BitmapSource src = null;
+        private void objKS_ColorFrameReady(object sender, ColorImageFrameReadyEventArgs e)
+        {
+            ColorImageFrame ImageParam = e.OpenColorImageFrame();
+            if (ImageParam == null) return;
+            byte[] ImageBits = new byte[ImageParam.PixelDataLength];
+            ImageParam.CopyPixelDataTo(ImageBits);
+
+            src = BitmapSource.Create(ImageParam.Width,
+                                      ImageParam.Height,
+                                      96, 96,
+                                      PixelFormats.Bgr32,
+                                      null,
+                                      ImageBits,
+                                      ImageParam.Width * ImageParam.BytesPerPixel);
+            image1.Source = src;
+        }
+
+        /********************************************************************************************************/
+        private void objKS_AllFramesReady(object sender, AllFramesReadyEventArgs e)
+        {
+            SkeletonFrame sf = e.OpenSkeletonFrame();
+            if (sf == null) return;
+            Skeleton[] skeletonData = new Skeleton[sf.SkeletonArrayLength];
+            sf.CopySkeletonDataTo(skeletonData);
+            using (DepthImageFrame depthImageFrame = e.OpenDepthImageFrame())
+            {
+                if (depthImageFrame != null)
+                {
+                    foreach (Skeleton sd in skeletonData)
+                    {
+                        if (sd.TrackingState == SkeletonTrackingState.Tracked)
+                        {
+                            Joint joint = sd.Joints[JointType.Head];
+
+                            DepthImagePoint depthPoint;
+                            depthPoint = depthImageFrame.MapFromSkeletonPoint(joint.Position);
+
+                            Point point = new Point((int)(image1.Height * depthPoint.X / depthImageFrame.Height), (int)(image1.Height * depthPoint.X / depthImageFrame.Height));
+                            double y = point.Y;
+                            textBlock1.Text = String.Format("Y : {1:0.00}", y);
+                            Canvas.SetTop(ellipse1, y);
+
+                            //현재 y좌표가 설정한 값보다 낮다면 아래 시작.
+                            if (y <= detect)
+                            {
+                                cctvImageSave();  //카메라 찍고 로그저장
+                                if (btnDetect.IsChecked == true)
+                                {
+                                    //문자보내는 api시작
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        // y좌표 설정버튼
+        private void btnSetting_Click(object sender, RoutedEventArgs e)
+        {
+            detect = double.Parse(txtY.Text);
+            MessageBox.Show("감지좌표가 " + detect.ToString() + "로 설정되었습니다.");
+        }
+        /********************************************************************************************************/
+        /********************************************************************************************************/
+        /********************************************************************************************************/
+        /********************************************************************************************************/
+        /********************************************************************************************************/
+        //기능
+
+        private void cctvImageSave()
+        {
+            DateTime time = DateTime.Now;
+            string fileName = time.ToString("yy/MM/dd/HH-mm-ss") + ".png";
+            if (src != null)
+            {
+                BitmapEncoder encoder = null;
+                encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(src));
+
+                FileStream fStream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+                encoder.Save(fStream);
+                fStream.Close();
+                System.Diagnostics.Process exe = new System.Diagnostics.Process();
+                exe.StartInfo.FileName = fileName;
+                exe.Start();
+            }
+        }
+
+        private void btnDetect_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
